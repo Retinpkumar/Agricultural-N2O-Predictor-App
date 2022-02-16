a3cf6709650b3695e10bafbb1627ee773645fa82
Retinpkumar <retinpkumar@gmail.com>
2022-02-16 12:01:24 +0530

    UPDATE setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
+[theme]\n\
+ textColor='#000000'\n\
+ backgroundColor='#D3ECDB'\n\
+ font='sans serif'\n\
+" > ~/.streamlit/config.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
